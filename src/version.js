17892ec64646457f2720191307f77f6a670e1ecd
Amjad Masad <amjad.masad@gmail.com>
2014-02-09 13:32:42 -0500

    checkin version for use in repl.it

---
+++ src/version.js
@@ -0,0 +1,11 @@
+/*
+ * BiwaScheme 0.6.2.dev - R6RS Scheme in JavaScript
+ *
+ * Copyright (c) 2007-2012 Yutaka HARA (http://www.biwascheme.org/)
+ * Licensed under the MIT license.
+ */
+
+var BiwaScheme = BiwaScheme || {};
+
+BiwaScheme.Version = "0.6.2.dev";
+BiwaScheme.GitCommit = "6645c10157e7b5772cef433e97918aa8351785ac";
